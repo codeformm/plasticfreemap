3f0cca0d886a1d88f681ee72fa3526f6f38df4aa
ishimasar <ishimasar@users.noreply.github.com>
2020-12-07 13:48:32 +0000

    deploy: 0a6f4bb331a017a7665d34647112fea452a65590

---
+++ _next/static/fWRupN6L9k32DnKezFsKS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F2d2f004d0b0562ba2776.css","static\u002Fchunks\u002Fpages\u002Findex-52812b5c63afb313ab15.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3f666bffbdfaa970940f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
